c2d7a182ae22e80b025adae8cd00c92cca999b3d
Ivan Eroshkin <erijj@mail.ru>
2022-01-10 18:36:38 +0100

    Cleaning up some code

--- pkg/store/a1ei/utils.go
+++ pkg/store/a1ei/utils.go
@@ -8,32 +8,6 @@ import (
 	"context"
 )
 
-//func GetEIjobByTypeID(ctx context.Context, s Store, eiTypeID string, ch chan<- *Entry) error {
-//	eiJobChEntries := make(chan *Entry)
-//	done := make(chan bool)
-//
-//	go func(eiJobTypeID string, ch chan<- *Entry) {
-//
-//		for entry := range eiJobChEntries {
-//			eiJobTypeID := entry.Value.(a1eistore.Value)
-//			if eiJobTypeID == eiTypeID {
-//				ch <- entry
-//			}
-//		}
-//		done <- true
-//	}(eiTypeID, ch)
-//
-//	err := s.Entries(ctx, eiJobChEntries)
-//	if err != nil {
-//		close(ch)
-//		return fmt.Errorf("no EI Job entries stored for Type %s", eiTypeID)
-//	}
-//
-//	<-done
-//	close(ch)
-//	return nil
-//}
-
 func GetEIjobByID(ctx context.Context, s Store, eiJobID string) (*Entry, error) {
 
 	a1eiKey := Key{
