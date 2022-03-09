78c6e8d84e54977576e4dca491f7fc7ca420ee09
Raphael Vicente Rosa <raphael@opennetworking.org>
2022-03-09 18:13:15 +0100

    Defines integration tests for 2 xApps
    Leaves support for install/uninstall xApp for next release.
    I.e., onos-a1t have a logic for consistency with onos-topo.

--- test/a1p/a1p.go
+++ test/a1p/a1p.go
@@ -25,123 +25,123 @@ func startA1TXapp(t *testing.T, xappName, policyTypeIDs string) *helm.HelmReleas
 	return a1txapp
 }
 
-// TestA1TPMService is the function for Helmit-based integration test
-func (s *TestSuite) TestA1TPMService(t *testing.T) {
+// // TestA1TPMService is the function for Helmit-based integration test
+// func (s *TestSuite) TestA1TPMService(t *testing.T) {
 
-	xAppName := "onos-a1txapp"
-	xAppPolicyTypeIDs := "ORAN_TrafficSteeringPreference_2.0.0"
+// 	xAppName := "onos-a1txapp"
+// 	xAppPolicyTypeIDs := "ORAN_TrafficSteeringPreference_2.0.0"
 
-	t.Log("A1T Policy Management suite test started")
+// 	t.Log("A1T Policy Management suite test started")
 
-	mgr, err := nonrtric.NewManager(utils.NonRTRicBaseURL, utils.NearRTRicBaseURL)
-	assert.NoError(t, err)
-	mgr.Run()
-	control := mgr.GetController()
+// 	mgr, err := nonrtric.NewManager(utils.NonRTRicBaseURL, utils.NearRTRicBaseURL)
+// 	assert.NoError(t, err)
+// 	mgr.Run()
+// 	control := mgr.GetController()
 
-	ctx, cancel := context.WithTimeout(context.Background(), utils.TestTimeout)
-	defer cancel()
+// 	ctx, cancel := context.WithTimeout(context.Background(), utils.TestTimeout)
+// 	defer cancel()
 
-	// 1. Before starting xApp with subscription to policy typeID
-	policyTypes, err := control.A1PMGetPolicytypes(ctx)
-	assert.NoError(t, err)
-	assert.ElementsMatch(t, []string{}, policyTypes)
-	t.Log("A1T Policy Management: Expected empty policy type IDs passed")
+// 	// 1. Before starting xApp with subscription to policy typeID
+// 	policyTypes, err := control.A1PMGetPolicytypes(ctx)
+// 	assert.NoError(t, err)
+// 	assert.ElementsMatch(t, []string{}, policyTypes)
+// 	t.Log("A1T Policy Management: Expected empty policy type IDs passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 2. Start xApp with a1t NBI enabled for policy management
-	a1txapp := startA1TXapp(t, xAppName, xAppPolicyTypeIDs)
-	time.Sleep(waitPeriod * time.Second)
+// 	// 2. Start xApp with a1t NBI enabled for policy management
+// 	a1txapp := startA1TXapp(t, xAppName, xAppPolicyTypeIDs)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 3. After starting xApp with subscription to policy typeID
-	policyTypes, err = control.A1PMGetPolicytypes(ctx)
-	assert.NoError(t, err)
-	assert.ElementsMatch(t, utils.ExpectedA1PMTypeIDs, policyTypes)
-	t.Log("A1T Policy Management: Expected PM type IDs passed")
+// 	// 3. After starting xApp with subscription to policy typeID
+// 	policyTypes, err = control.A1PMGetPolicytypes(ctx)
+// 	assert.NoError(t, err)
+// 	assert.ElementsMatch(t, utils.ExpectedA1PMTypeIDs, policyTypes)
+// 	t.Log("A1T Policy Management: Expected PM type IDs passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 4. Retrieves policy typeID schema
-	policySchema, err := control.A1PMGetPolicytypesPolicyTypeId(ctx, utils.PolicyTypeId)
-	// t.Log("Received policySchema ", policySchema)
-	assert.NoError(t, err)
+// 	// 4. Retrieves policy typeID schema
+// 	policySchema, err := control.A1PMGetPolicytypesPolicyTypeId(ctx, utils.PolicyTypeId)
+// 	// t.Log("Received policySchema ", policySchema)
+// 	assert.NoError(t, err)
 
-	// 5. Validates policy typeID schema
-	err = utils.ValidateSchema(utils.ExpectedPolicyObject, policySchema)
-	assert.NoError(t, err)
-	t.Log("A1T Policy Management: Expected policy type schema passed")
+// 	// 5. Validates policy typeID schema
+// 	err = utils.ValidateSchema(utils.ExpectedPolicyObject, policySchema)
+// 	assert.NoError(t, err)
+// 	t.Log("A1T Policy Management: Expected policy type schema passed")
 
-	// 6. Query policy IDs with a particular policy typeID
-	existingPolicyIDs, err := control.A1PMGetPolicytypesPolicyTypeIdPolicies(ctx, utils.PolicyTypeId)
-	assert.NoError(t, err)
-	assert.ElementsMatch(t, []string{}, existingPolicyIDs)
-	t.Log("A1T Policy Management: Expected policy IDs for typeID passed")
+// 	// 6. Query policy IDs with a particular policy typeID
+// 	existingPolicyIDs, err := control.A1PMGetPolicytypesPolicyTypeIdPolicies(ctx, utils.PolicyTypeId)
+// 	assert.NoError(t, err)
+// 	assert.ElementsMatch(t, []string{}, existingPolicyIDs)
+// 	t.Log("A1T Policy Management: Expected policy IDs for typeID passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 7. Creates policy
-	err = control.A1PMPutPolicytypesPolicyTypeIdPoliciesPolicyId(ctx, utils.PolicyTypeId, utils.PolicyId, utils.NotifyURL, utils.ExpectedPolicyObject)
-	assert.NoError(t, err)
-	t.Log("A1T Policy Management: Expected policy create passed")
+// 	// 7. Creates policy
+// 	err = control.A1PMPutPolicytypesPolicyTypeIdPoliciesPolicyId(ctx, utils.PolicyTypeId, utils.PolicyId, utils.NotifyURL, utils.ExpectedPolicyObject)
+// 	assert.NoError(t, err)
+// 	t.Log("A1T Policy Management: Expected policy create passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 8. Query policy IDs with a particular policy typeID (must be list with expected policy ID)
-	PolicyIDs, err := control.A1PMGetPolicytypesPolicyTypeIdPolicies(ctx, utils.PolicyTypeId)
-	assert.NoError(t, err)
-	assert.ElementsMatch(t, utils.ExpectedA1PMPolicyIDs, PolicyIDs)
-	t.Log("A1T Policy Management: Expected policy IDs for typeID passed")
+// 	// 8. Query policy IDs with a particular policy typeID (must be list with expected policy ID)
+// 	PolicyIDs, err := control.A1PMGetPolicytypesPolicyTypeIdPolicies(ctx, utils.PolicyTypeId)
+// 	assert.NoError(t, err)
+// 	assert.ElementsMatch(t, utils.ExpectedA1PMPolicyIDs, PolicyIDs)
+// 	t.Log("A1T Policy Management: Expected policy IDs for typeID passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 7. Query policy status
-	policyStatus, err := control.A1PMGetPolicytypesPolicyTypeIdPoliciesPolicyIdStatus(ctx, utils.PolicyTypeId, utils.PolicyId)
-	assert.NoError(t, err)
-	err = utils.ValidateSchema(policyStatus, utils.ExpectedPolicyStatusSchema)
-	assert.NoError(t, err)
-	t.Log("A1T Policy Management: Expected policy status passed")
+// 	// 7. Query policy status
+// 	policyStatus, err := control.A1PMGetPolicytypesPolicyTypeIdPoliciesPolicyIdStatus(ctx, utils.PolicyTypeId, utils.PolicyId)
+// 	assert.NoError(t, err)
+// 	err = utils.ValidateSchema(policyStatus, utils.ExpectedPolicyStatusSchema)
+// 	assert.NoError(t, err)
+// 	t.Log("A1T Policy Management: Expected policy status passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 8. Query policy
-	policyObject, err := control.A1PMGetPolicytypesPolicyTypeIdPoliciesPolicyId(ctx, utils.PolicyTypeId, utils.PolicyId)
-	assert.NoError(t, err)
-	err = utils.ValidateSchema(policyObject, utils.ExpectedPolicySchema)
-	assert.NoError(t, err)
-	t.Log("A1T Policy Management: Expected policy object query passed")
+// 	// 8. Query policy
+// 	policyObject, err := control.A1PMGetPolicytypesPolicyTypeIdPoliciesPolicyId(ctx, utils.PolicyTypeId, utils.PolicyId)
+// 	assert.NoError(t, err)
+// 	err = utils.ValidateSchema(policyObject, utils.ExpectedPolicySchema)
+// 	assert.NoError(t, err)
+// 	t.Log("A1T Policy Management: Expected policy object query passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 9. Delete policy
-	err = control.A1PMDeletePolicytypesPolicyTypeIdPoliciesPolicyId(ctx, utils.PolicyTypeId, utils.PolicyId)
-	assert.NoError(t, err)
-	t.Log("A1T Policy Management: Expected policy delete passed")
+// 	// 9. Delete policy
+// 	err = control.A1PMDeletePolicytypesPolicyTypeIdPoliciesPolicyId(ctx, utils.PolicyTypeId, utils.PolicyId)
+// 	assert.NoError(t, err)
+// 	t.Log("A1T Policy Management: Expected policy delete passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	// 10. Query policy IDs with a particular policy typeID
-	emptyPolicyIDs, err := control.A1PMGetPolicytypesPolicyTypeIdPolicies(ctx, utils.PolicyTypeId)
-	assert.NoError(t, err)
-	assert.ElementsMatch(t, []string{}, emptyPolicyIDs)
-	t.Log("A1T Policy Management: Expected policy IDs for typeID passed")
+// 	// 10. Query policy IDs with a particular policy typeID
+// 	emptyPolicyIDs, err := control.A1PMGetPolicytypesPolicyTypeIdPolicies(ctx, utils.PolicyTypeId)
+// 	assert.NoError(t, err)
+// 	assert.ElementsMatch(t, []string{}, emptyPolicyIDs)
+// 	t.Log("A1T Policy Management: Expected policy IDs for typeID passed")
 
-	time.Sleep(waitPeriod * time.Second)
+// 	time.Sleep(waitPeriod * time.Second)
 
-	t.Log("A1T Policy Management suite test finished")
+// 	t.Log("A1T Policy Management suite test finished")
 
-	err = a1txapp.Uninstall()
-	assert.NoError(t, err, "could not uninstall a1txapp %v", err)
-	t.Log("A1T xApp stopped")
+// 	err = a1txapp.Uninstall()
+// 	assert.NoError(t, err, "could not uninstall a1txapp %v", err)
+// 	t.Log("A1T xApp stopped")
 
-	mgr.End()
-	t.Log("Ended A1T non-RT RIC manager")
+// 	mgr.End()
+// 	t.Log("Ended A1T non-RT RIC manager")
 
-}
+// }
 
-// TestA1TPMService is the function for Helmit-based integration test
+// TestA1TPMTwoxAppService is the function for Helmit-based integration test
+// for 2 xApps interacting with onos-a1t
+// TODO: Test case that supports xApp uninstall/install TBD
 func (s *TestSuite) TestA1TPMTwoxAppService(t *testing.T) {
-
-	time.Sleep(5 * time.Second)
 
 	xApp1Name := "onos-a1txapp2"
 	xApp1PolicyTypeIDs := "ORAN_TrafficSteeringPreference_2.0.0"
